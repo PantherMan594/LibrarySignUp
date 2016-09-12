25f1a52cab2c0f9702128bf86990467ad7fa407c
David Shen <me@pantherman594.com>
2016-09-11 23:42:37 -0400

    Initial commit

---
+++ src/com/pantherman594/librarysignup/LibrarySignUp.java
@@ -0,0 +1,128 @@
+package com.pantherman594.librarysignup;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by david on 9/11.
+ */
+
+public class LibrarySignUp extends Application {
+    private static LibrarySignUp instance;
+
+    private boolean runScheduler;
+    private Stage primaryStage;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        instance = this;
+        this.primaryStage = primaryStage;
+
+        Map<Integer, Integer> studies = new HashMap<>();
+        Set<ComboBox> comboBoxes = new HashSet<>();
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_LEFT);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 450, 300);
+
+        Text title = new Text("\tPlease select your studies:");
+        title.setFont(Font.font("Lato, Tahoma", FontWeight.NORMAL, 18));
+
+        grid.add(title, 0, 0, 3, 1);
+
+        for (int i = 1; i <= 6; i++) {
+            Label day = new Label("Day " + i + ": ");
+            grid.add(day, 0, i);
+
+            ComboBox dayBox = new ComboBox();
+            dayBox.getItems().addAll(
+                    "None",
+                    "R1",
+                    "R2",
+                    "R3",
+                    "R4",
+                    "R6",
+                    "R7"
+            );
+            dayBox.setValue("None");
+
+            final int x = i;
+            dayBox.valueProperty().addListener((obs, wasSelected, isNowSelected) -> {
+                if (isNowSelected != "None") {
+                    studies.put(x, Integer.valueOf(((String) isNowSelected).substring(1)));
+                } else {
+                    studies.remove(x);
+                }
+            });
+            grid.add(dayBox, 1, i);
+            comboBoxes.add(dayBox);
+        }
+
+        TextField email = new TextField();
+        email.setPromptText("Enter your email.");
+        email.setPrefColumnCount(20);
+        grid.add(email, 2, 3);
+
+        TextField password = new TextField();
+        password.setPromptText("Enter your password.");
+        password.setPrefColumnCount(20);
+        grid.add(password, 2, 4);
+
+        HBox submitBox = new HBox(10);
+        submitBox.setAlignment(Pos.BOTTOM_RIGHT);
+
+        Button submit = new Button("Submit");
+        runScheduler = true;
+        submit.setOnAction(new Scheduler(email, password, title, submit, studies, comboBoxes));
+
+        submitBox.getChildren().add(submit);
+        grid.add(submitBox, 2, 5);
+
+        primaryStage.setTitle("Library Sign Up");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        scene.getWindow().setOnCloseRequest(event -> exit(0));
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    boolean shouldRunScheduler() {
+        return runScheduler;
+    }
+
+    void exit(int status) {
+        runScheduler = false;
+        primaryStage.hide();
+        System.exit(status);
+    }
+
+    static LibrarySignUp getInstance() {
+        return instance;
+    }
+}
---
+++ src/com/pantherman594/librarysignup/Scheduler.java
@@ -0,0 +1,204 @@
+package com.pantherman594.librarysignup;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by david on 9/11.
+ */
+class Scheduler implements EventHandler<ActionEvent> {
+    private TextField email;
+    private TextField password;
+    private Text title;
+    private Button submit;
+    private Set<ComboBox> comboBoxes;
+    private Map<Long, String> holidays;
+    private Map<Integer, Integer> studies;
+
+    Scheduler(TextField email, TextField password, Text title, Button submit, Map<Integer, Integer> studies, Set<ComboBox> comboBoxes) {
+        this.email = email;
+        this.password = password;
+        this.title = title;
+        this.submit = submit;
+        this.studies = studies;
+        this.comboBoxes = comboBoxes;
+
+        holidays = new HashMap<>();
+        addHolidays();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (email.getText() == null || email.getText().isEmpty() || password.getText() == null || password.getText().isEmpty()) {
+            title.setText("\tError: Invalid email/password.");
+            return;
+        }
+        title.setText("\tLibrary Sign Up is running... (minimize this)");
+        for (ComboBox comboBox : comboBoxes) {
+            comboBox.setDisable(true);
+        }
+
+        submit.setText("Quit");
+        submit.setOnAction(ev -> LibrarySignUp.getInstance().exit(0));
+
+        new Thread(() -> {
+            while (LibrarySignUp.getInstance().shouldRunScheduler()) {
+                Calendar date = Calendar.getInstance();
+                date.add(Calendar.DAY_OF_MONTH, 1);
+                date.set(Calendar.HOUR_OF_DAY, 18);
+                date.set(Calendar.MINUTE, 59);
+                date.set(Calendar.SECOND, 45);
+                date.set(Calendar.MILLISECOND, 0);
+
+                try {
+                    Thread.sleep(date.getTimeInMillis() - System.currentTimeMillis());
+                } catch (InterruptedException ignored) {}
+
+                long tomorrow = LocalDate.now().toEpochDay() + 1;
+                int sixDay = getSixDay(getSchoolDays(tomorrow));
+                if (isSchoolDay(tomorrow) && studies.keySet().contains(sixDay)) {
+                    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                        int control = KeyEvent.VK_CONTROL;
+                        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                            control = KeyEvent.VK_META;
+                        }
+
+                        Keyboard k = null;
+                        try {
+                            k = new Keyboard();
+                        } catch (AWTException ignored) {
+                            title.setText("Error: Unable to initialize keyboard robot.");
+                            return;
+                        }
+
+                        try {
+                            Desktop.getDesktop().browse(new URI("https://pickatime.com/client?logout=on&ven=11607876"));
+                            Thread.sleep(1000);
+                            k.doType(control, KeyEvent.VK_W);
+                            Desktop.getDesktop().browse(new URI("https://pickatime.com/client?ven=11607876&email=" + email.getText()));
+                            Thread.sleep(5000);
+
+                            k.doType(control, KeyEvent.VK_F);
+                            Thread.sleep(1000);
+                            k.type("onal pass");
+                            Thread.sleep(1000);
+                            k.doType(KeyEvent.VK_ESCAPE);
+                            k.type("\t");
+                            k.type(password.getText());
+                            k.type("\n");
+
+                            Thread.sleep(5000);
+
+                            k.doType(control, KeyEvent.VK_F);
+                            Thread.sleep(1000);
+                            k.type("st");
+                            k.doType(KeyEvent.VK_ESCAPE);
+                            k.type("\n");
+
+                            Thread.sleep(5000);
+
+                            k.doType(control, KeyEvent.VK_F);
+                            Thread.sleep(1000);
+
+                            String idNum = "55 ";
+                            switch(studies.get(sixDay)) {
+                                case 2: idNum = "59 "; break;
+                                case 3: idNum = "48 "; break;
+                                case 4: idNum = "37 "; break;
+                                case 6: idNum = "42 "; break;
+                                case 7: idNum = "30 "; break;
+                            }
+                            k.type(idNum);
+                            Thread.sleep(1000);
+                            k.type("\n");
+                            Thread.sleep(1000);
+                            k.doType(KeyEvent.VK_ESCAPE);
+                            k.type("\n");
+
+                            Thread.sleep(5000);
+
+                            k.doType(control, KeyEvent.VK_F);
+                            Thread.sleep(1000);
+                            k.type("cr");
+                            k.doType(KeyEvent.VK_ESCAPE);
+                            k.type("\n");
+
+                            k.doType(control, KeyEvent.VK_W);
+                            Desktop.getDesktop().browse(new URI("https://pickatime.com/client?logout=on&ven=11607876"));
+                            Thread.sleep(1000);
+                            k.doType(control, KeyEvent.VK_W);
+                        } catch (IOException | URISyntaxException | InterruptedException ignored) {}
+                        System.exit(0);
+                    } else {
+                        title.setText("Error: Unable to open browser.");
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private int getSchoolDays(long date) {
+        int schoolDays = 1;
+        for (long i = 17052; i < date; i++) {
+            if (isSchoolDay(i)) {
+                schoolDays++;
+            }
+        }
+        return schoolDays;
+    }
+
+    private boolean isSchoolDay(long date) {
+        return date % 7 != 2 && date % 7 != 3 && !holidays.keySet().contains(date);
+    }
+
+    private int getSixDay(int schoolDays) {
+        return schoolDays % 6 == 0 ? 6 : schoolDays % 6;
+    }
+
+    private void addHolidays() {
+        addHoliday(2016, 10, 10, "Columbus Day");
+        addHoliday(2016, 11, 11, "Veterans' Day");
+        addHoliday(2016, 11, 24, "Thanksgiving Recess");
+        addHoliday(2016, 11, 25, "Thanksgiving Recess");
+        addHoliday(2016, 12, 23, "Winter Recess");
+        addHoliday(2016, 12, 26, "Winter Recess");
+        addHoliday(2016, 12, 27, "Winter Recess");
+        addHoliday(2016, 12, 28, "Winter Recess");
+        addHoliday(2016, 12, 29, "Winter Recess");
+        addHoliday(2016, 12, 30, "Winter Recess");
+        addHoliday(2017, 1, 2, "Winter Recess");
+        addHoliday(2017, 1, 3, "Winter Recess");
+        addHoliday(2017, 1, 16, "M. L. King Jr. Day");
+        addHoliday(2017, 2, 20, "Presidents' Day");
+        addHoliday(2017, 2, 21, "February Recess");
+        addHoliday(2017, 2, 22, "February Recess");
+        addHoliday(2017, 2, 23, "February Recess");
+        addHoliday(2017, 2, 24, "February Recess");
+        addHoliday(2017, 4, 14, "Good Friday");
+        addHoliday(2017, 4, 17, "Patriots' Day");
+        addHoliday(2017, 4, 18, "Spring Recess");
+        addHoliday(2017, 4, 19, "Spring Recess");
+        addHoliday(2017, 4, 20, "Spring Recess");
+        addHoliday(2017, 4, 21, "Spring Recess");
+        addHoliday(2017, 5, 29, "Memorial Day");
+    }
+
+    private void addHoliday(int year, int month, int day, String name) {
+        holidays.put(LocalDate.of(year, month, day).toEpochDay(), name);
+    }
+}
