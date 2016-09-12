92b9b4d947ba666f79e5f794b603c5af5d1c403f
David Shen <me@pantherman594.com>
2016-09-11 23:53:29 -0400

    Create README.md and LICENSE.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# LibrarySignUp
+Automatically sign up for BLS library.
+
+## Use:
+1. Install Java if you don't have it
+2. Download the latest version from [here](https://github.com/PantherMan594/LibrarySignUp/releases)
+3. Run the LibrarySignUp.jar file
+4. Input your studies, email, and password (note: use your pickatime email and password. This information will never leave your computer, and is _only_ used to automatically sign in. If you want to look into the source, you can see that I'll _never_ have access to either your email or your password)
+5. Press submit, make sure there aren't any errors, and minimize the window (note: your computer needs to be on during the sign up time for this to work)
+6. When the time comes, it will automatically fill in _everything_. Just let it do its job, but if it messes up you can do it manually.
--- src/com/pantherman594/librarysignup/LibrarySignUp.java
+++ src/com/pantherman594/librarysignup/LibrarySignUp.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2016 David Shen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.pantherman594.librarysignup;
 
 import javafx.application.Application;
@@ -29,6 +39,14 @@ public class LibrarySignUp extends Application {
 
     private boolean runScheduler;
     private Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    static LibrarySignUp getInstance() {
+        return instance;
+    }
 
     @SuppressWarnings("unchecked")
     @Override
@@ -107,11 +125,6 @@ public class LibrarySignUp extends Application {
         scene.getWindow().setOnCloseRequest(event -> exit(0));
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     boolean shouldRunScheduler() {
         return runScheduler;
     }
@@ -120,9 +133,5 @@ public class LibrarySignUp extends Application {
         runScheduler = false;
         primaryStage.hide();
         System.exit(status);
-    }
-
-    static LibrarySignUp getInstance() {
-        return instance;
     }
 }
